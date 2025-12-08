62f71fc5b7172b1a1401bee23b4b07fb23140d7b
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2025-12-08 10:39:03 +0100

    Day eight, part 2

--- 8_2.py
+++ 8_2.py
@@ -1,5 +1,6 @@
 import math
 from dataclasses import dataclass
+from sortedcontainers import SortedSet
 
 data = ""
 with open("./input.txt", "r") as f:
@@ -14,30 +15,27 @@ class Coord:
     def dist(self, other):
         return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2 + (self.z - other.z)**2)
 
-to_connect = 1000
-
 coords = list(map(lambda x: Coord(int(x[0]), int(x[1]), int(x[2])), map(lambda x: x.split(","), data.split("\n"))))
 
 distances = dict()
 
 for coord in coords:
-    d = dict()
+    d = SortedSet()
     for coord2 in coords:
         if coord == coord2:
             continue
 
-        d[coord2] = coord.dist(coord2)
+        d.add((coord.dist(coord2), coord2)) 
 
     distances[coord] = d
 
 junctions = []
 
-for i in range(to_connect):
-    print(f"{i=}")
+i = 0
+while True:
     s = None
     for c, d in distances.items():
-        smc = min(d, key=d.get)
-        smd = d[smc]
+        smd, smc = d[0]
         if s == None or smd < s[2]:
             s = (c, smc, smd)
 
@@ -58,8 +56,8 @@ for i in range(to_connect):
         if rcurr != None and lcurr != None:
             break
 
-    del distances[r][l]
-    del distances[l][r]
+    distances[r].remove((d, l))
+    distances[l].remove((d, r))
 
     if rcurr == None and lcurr == None:
         junctions.append(set([r, l]))
@@ -72,8 +70,9 @@ for i in range(to_connect):
             rcurr.add(j)
         lcurr.clear()
 
-final = list(map(len, junctions))
-filtered = list(filter(lambda x: x != 0, final))
-filtered.sort()
-print(final, filtered)
-print(math.prod(filtered[-3:]))
+    filtered = list(filter(lambda x: x != 0, map(len, junctions)))
+    if len(filtered) == 1 and filtered[0] == len(coords):
+        print(r.x * l.x)
+        break
+    
+    i += 1
